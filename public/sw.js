f5894b00beb6d91797f1d90b586d535a0c3c3692
yokinanya <yokinaaa@outlook.com>
2024-08-13 16:34:16 +0800

    怎么会是呢

--- public/sw.js
+++ public/sw.js
@@ -38,7 +38,6 @@ const CACHE_STRATEGY = {
     css_js: 'cacheFirst',
     images: 'cacheFirst',
     fonts: 'cacheFirst',
-    media: 'cacheFirst',
 }
 
 const CACHE_BLACKLIST = [
@@ -641,9 +640,6 @@ self.addEventListener(
                     break;
                 case 'font':
                     cachingStrategyType = CACHE_STRATEGY.fonts;
-                    break;
-                case 'media':
-                    cachingStrategyType = CACHE_STRATEGY.media;
                     break;
                 // All `XMLHttpRequest` or `fetch()` calls where
                 // `Request.destination` is the empty string default value
